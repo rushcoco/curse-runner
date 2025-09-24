cc923feefb99490b6aa0cb7c8047fcceb564facf
cocorush <coco.rush@pm.me>
2025-09-24 13:28:13 +0200

    Added commeents for grappling and added layer for bloodorb prefab

--- Assets/Player/Grappling.cs
+++ Assets/Player/Grappling.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 
 public class Grappling : MonoBehaviour
 {
+    // Needs to recognize which objects should be hit and which shouldn't be hit
+    // - layers?
+    // - get component "Blood Prefab" check?
+    
+    // Needs a thing to hit the other Objects and do check
+    // - Raycast?
+    // - Physics
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
